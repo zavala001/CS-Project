53123c42c7f41850433daf7271fe7c1a54635a47
valeriaramirez12345 <2010210@newton.pe>
2024-07-11 09:01:06 -0500

    Drinks.java

---
+++ Drinks.java
@@ -0,0 +1,8 @@
+ class Drinks extends Recipes {
+        public Drinks(String name, String description, String ingredients, String preptime, String directions, String nutritionfacts) {
+            super(name, description, ingredients, preptime, directions, nutritionfacts);
+
+
+
+        }
+    }
