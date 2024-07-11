c82f99709ef1ffe4547415356f479d916fe7d8a2
valeriaramirez12345 <2010210@newton.pe>
2024-07-11 09:00:15 -0500

    Snacks.java

---
+++ Snacks.java
@@ -0,0 +1,5 @@
+class Snacks extends Recipes {
+    public Snacks(String name, String description, String ingredients, String preptime, String directions, String nutritionfacts) {
+        super(name, description, ingredients, preptime, directions, nutritionfacts);
+    }
+}
