b5c2ec28fffd88c03997a59df713d2d57ac94ac4
valeriaramirez12345 <2010210@newton.pe>
2024-07-11 09:00:41 -0500

    Sandwiches.java

---
+++ Sandwiches.java
@@ -0,0 +1,8 @@
+ class Sandwiches extends Recipes {
+        public Sandwiches(String name, String description, String ingredients, String preptime, String directions, String nutritionfacts) {
+            super(name, description, ingredients, preptime, directions, nutritionfacts);
+
+
+
+        }
+    }
